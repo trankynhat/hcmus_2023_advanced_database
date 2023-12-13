6a1a2db66ac6cb31e5be24bf7415263b0bdf2434
NguyenTrongThuan <ntthuan060102.work@gmail.com>
2023-12-13 16:33:41 +0700

    update mock branch

--- Scripts/Mock/branch.sql
+++ Scripts/Mock/branch.sql
@@ -1,50 +1,104 @@
-insert into branch (address) values ('032 Surrey Court');
-insert into branch (address) values ('929 Continental Pass');
-insert into branch (address) values ('02 Pankratz Hill');
-insert into branch (address) values ('19 Welch Alley');
-insert into branch (address) values ('5656 Grover Court');
-insert into branch (address) values ('5 Kropf Point');
-insert into branch (address) values ('26 Cottonwood Plaza');
-insert into branch (address) values ('8 Monterey Circle');
-insert into branch (address) values ('150 Blaine Crossing');
-insert into branch (address) values ('320 5th Hill');
-insert into branch (address) values ('312 Parkside Center');
-insert into branch (address) values ('3412 Mayer Parkway');
-insert into branch (address) values ('8564 Declaration Drive');
-insert into branch (address) values ('481 Springview Terrace');
-insert into branch (address) values ('3234 Clyde Gallagher Street');
-insert into branch (address) values ('511 American Junction');
-insert into branch (address) values ('71 Mallard Way');
-insert into branch (address) values ('8 Bunker Hill Way');
-insert into branch (address) values ('0033 Esch Court');
-insert into branch (address) values ('75480 Hoffman Crossing');
-insert into branch (address) values ('68451 Meadow Valley Place');
-insert into branch (address) values ('918 Gulseth Trail');
-insert into branch (address) values ('99 Westridge Pass');
-insert into branch (address) values ('79 Vera Trail');
-insert into branch (address) values ('4416 Porter Court');
-insert into branch (address) values ('1637 Talmadge Terrace');
-insert into branch (address) values ('05306 Monica Street');
-insert into branch (address) values ('5673 Jackson Way');
-insert into branch (address) values ('071 Dovetail Drive');
-insert into branch (address) values ('27 Sutteridge Court');
-insert into branch (address) values ('6 Glacier Hill Junction');
-insert into branch (address) values ('37968 Basil Park');
-insert into branch (address) values ('06690 Bashford Center');
-insert into branch (address) values ('1698 Westport Park');
-insert into branch (address) values ('943 Loftsgordon Circle');
-insert into branch (address) values ('87727 Gerald Place');
-insert into branch (address) values ('609 Bobwhite Circle');
-insert into branch (address) values ('948 Moulton Pass');
-insert into branch (address) values ('9 Mallard Point');
-insert into branch (address) values ('58 Badeau Street');
-insert into branch (address) values ('537 Carioca Circle');
-insert into branch (address) values ('50 Carberry Circle');
-insert into branch (address) values ('390 Graceland Alley');
-insert into branch (address) values ('680 Cascade Parkway');
-insert into branch (address) values ('7563 Hauk Parkway');
-insert into branch (address) values ('51668 Judy Court');
-insert into branch (address) values ('8 Warrior Place');
-insert into branch (address) values ('0 Dovetail Avenue');
-insert into branch (address) values ('52 Delladonna Circle');
-insert into branch (address) values ('68 Mcguire Center');
+SET IDENTITY_INSERT [dbo].[branch] ON 
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (0, 36, N'032 Surrey Court')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (1, 11, N'929 Continental Pass')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (2, 55, N'02 Pankratz Hill')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (3, 17, N'19 Welch Alley')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (4, 59, N'5656 Grover Court')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (5, 47, N'5 Kropf Point')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (6, 90, N'26 Cottonwood Plaza')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (7, 13, N'8 Monterey Circle')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (8, 64, N'150 Blaine Crossing')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (9, 54, N'320 5th Hill')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (10, 15, N'312 Parkside Center')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (11, 43, N'3412 Mayer Parkway')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (12, 37, N'8564 Declaration Drive')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (13, 41, N'481 Springview Terrace')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (14, 40, N'3234 Clyde Gallagher Street')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (15, 93, N'511 American Junction')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (16, 21, N'71 Mallard Way')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (17, 8, N'8 Bunker Hill Way')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (18, 27, N'0033 Esch Court')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (19, 86, N'75480 Hoffman Crossing')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (20, 66, N'68451 Meadow Valley Place')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (21, 111, N'918 Gulseth Trail')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (22, 98, N'99 Westridge Pass')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (23, 14, N'79 Vera Trail')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (24, 80, N'4416 Porter Court')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (25, 10, N'1637 Talmadge Terrace')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (26, 32, N'05306 Monica Street')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (27, 96, N'5673 Jackson Way')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (28, 48, N'071 Dovetail Drive')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (29, 49, N'27 Sutteridge Court')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (30, 6, N'6 Glacier Hill Junction')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (31, 18, N'37968 Basil Park')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (32, 1, N'06690 Bashford Center')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (33, 118, N'1698 Westport Park')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (34, 123, N'943 Loftsgordon Circle')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (35, 19, N'87727 Gerald Place')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (36, 88, N'609 Bobwhite Circle')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (37, 22, N'948 Moulton Pass')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (38, 50, N'9 Mallard Point')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (39, 20, N'58 Badeau Street')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (40, 82, N'537 Carioca Circle')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (41, 5, N'50 Carberry Circle')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (42, 3, N'390 Graceland Alley')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (43, 60, N'680 Cascade Parkway')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (44, 2, N'7563 Hauk Parkway')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (45, 68, N'51668 Judy Court')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (46, 53, N'8 Warrior Place')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (47, 12, N'0 Dovetail Avenue')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (48, 7, N'52 Delladonna Circle')
+GO
+INSERT [dbo].[branch] ([id], [admin_id], [address]) VALUES (49, 4, N'68 Mcguire Center')
+GO
+SET IDENTITY_INSERT [dbo].[branch] OFF
+GO
