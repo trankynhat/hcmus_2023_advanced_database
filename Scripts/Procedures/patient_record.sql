ac0220470daeca44eddf6fbbc235101b85e3322d
tinhobe159 <tinhobe158@gmail.com>
2023-12-18 14:07:30 +0700

    thông tin thanh toán

--- Scripts/Procedures/patient_record.sql
+++ Scripts/Procedures/patient_record.sql
@@ -5,7 +5,7 @@ BEGIN
     SELECT citizen_id, full_name, phone_number, gender, email, date_of_birth, permanent_address, general_info_about_oral_health, note
     FROM patient_record
     WHERE (
-        @search_text IS NULL 
+        @search_text IS NULL
         OR citizen_id LIKE '%' + @search_text + '%'
         OR full_name LIKE '%' + @search_text + '%'
         OR phone_number LIKE '%' + @search_text + '%'
@@ -18,7 +18,8 @@ GO
 CREATE PROC xem_chi_tiet_benh_nhan(@citizen_id NVARCHAR(150))
 AS
 BEGIN
-    SELECT TOP 1 citizen_id, full_name, phone_number, gender, email, date_of_birth, permanent_address, general_info_about_oral_health, note
+    SELECT TOP 1
+        citizen_id, full_name, phone_number, gender, email, date_of_birth, permanent_address, general_info_about_oral_health, note
     FROM patient_record
     WHERE citizen_id = @citizen_id
 END
@@ -306,13 +307,68 @@ GO
 
 -- get_treatment_phases_by_treatment_plan_id
 CREATE PROC get_treatment_phases_by_treatment_plan_id(@plan_id INT)
-AS 
+AS
 BEGIN
     SELECT T.id, T.description, T.treatment_fee, T.treatment_date, T.payment_method_code, T.payment_id, T.dentist_id
     FROM treatment T
-    RIGHT JOIN treatment_phase TP
-    ON T.id = TP.treatment_id
+        RIGHT JOIN treatment_phase TP
+        ON T.id = TP.treatment_id
     WHERE TP.treatment_plan_id = @plan_id
 END
 GO
 
+-- Tại Hồ sơ bệnh nhân, 
+-- các nha sĩ có thể xem được danh sách các thanh toán của bệnh nhân 
+-- bao gồm tên nha sĩ phụ trách các điều trị, 
+-- tổng tiền cần thanh toán và 
+-- ngày thực hiện thanh toán.
+
+
+CREATE PROC  danh_sach_cac_thanh_toan_cua_benh_nhan
+    (@citizen_id NVARCHAR(36))
+AS
+BEGIN
+    SELECT d1.full_name, p.total_treatment_fee, p.date_of_payment
+    FROM payment AS p
+        LEFT JOIN treatment AS t ON p.id = t.payment_id
+        LEFT JOIN (
+        SELECT d.user_id, u.full_name
+        FROM dentist AS d
+            LEFT JOIN [user] AS u ON d.user_id = u.id
+    ) AS d1 ON d1.user_id = t.dentist_id
+    WHERE t.citizen_id = @citizen_id;
+
+END
+GO
+
+-- Thông tin chi tiết của mỗi thanh toán gồm 
+-- ngày giao dịch, 
+-- người thanh toán, 
+-- tổng tiền cần thanh toán, 
+-- tiền đã trả, 
+-- tiền thối, 
+-- loại thanh toán (tiền mặt hoặc thanh toán online), 
+-- ghi chú và 
+
+CREATE PROC  thong_tin_chi_tiet_thanh_toan_cua_benh_nhan
+    (@payment_id VARCHAR(36))
+AS
+BEGIN
+    SELECT p.date_of_payment, p.total_treatment_fee, p.change, pm.name, p.note
+    FROM payment as p
+        LEFT JOIN payment_method as pm ON pm.code = p.payment_method_code
+    WHERE id = @payment_id
+
+END
+GO
+
+-- danh sách các điều trị cần thanh toán,
+-- mỗi điều trị gồm các thông tin: 
+-- mã điều trị, 
+-- mô tả, 
+-- phí và 
+-- ngày điều trị, 
+-- hình thức thanh toán (tiền mặt, online). 
+-- Nếu bệnh nhân thanh toán tiền mặt, 
+-- nhân viên lễ tân/quản trị viên sẽ nhập vào mục thanh toán trong hồ
+-- sơ bệnh nhân.
