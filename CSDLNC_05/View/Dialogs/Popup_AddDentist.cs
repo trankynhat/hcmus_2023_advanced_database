549c2c44a4b863ea1e8eefa28c00fa5e5991fade
TranKyNhat <nhattramking@gmail.com>
2024-01-07 21:23:14 +0700

    màn hình quản lý hệ thống

---
+++ CSDLNC_05/View/Dialogs/Popup_AddDentist.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSDLNC_05.View.Dialogs
+{
+    public partial class Popup_AddDentist : Form
+    {
+        public Popup_AddDentist()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Popup_AddDentist_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
